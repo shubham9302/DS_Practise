05dd3047acf969abc0230998ac59d2f1a06a7e67
shubham920816 <shubhaM1674@>
2021-11-24 21:47:38 +0530

    Recursion + Array

---
+++ arrayadt/arraysearch.py
@@ -0,0 +1,66 @@
+import array
+from arrayadt.implementarrayadt import ArrayADT
+
+
+class ArraySearch(ArrayADT):
+
+    def __init__(self):
+        super().__init__()
+        self.data = array.array('i', [2, 3, 6, 8, 9, 12, 14])
+
+    def linearsearch(self, element: int):
+        c = None
+        for i in range(len(self.array)):
+            if self.array[i] == element:
+                c = i
+        return c
+
+    def sortArray(self):
+        self.array.sort()
+
+    def binarysearchrecursion(self, element, low, high):
+        print("low", low)
+        print("high", high)
+        mid = int((low + high) / 2)
+        print("mid", mid)
+        if self.data[mid] != element:
+            if self.data[mid] >= element:
+                high = mid - 1
+                print("left loop")
+                print("low", low)
+                print("high", high)
+                print("#####")
+                return self.binarysearchrecursion(element, low, high)
+            else:
+                low = mid + 1
+                print("right loop")
+                print("low", low)
+                print("high", high)
+                print("#####")
+                return self.binarysearchrecursion(element, low, high)
+        else:
+            return mid
+
+    def binarysearchloop(self, element, low=0, high=0):
+
+        for i in range(0, len(self.data)):
+            mid = int((low + high) / 2)
+            if self.data[mid] == element:
+                return mid
+            elif self.data[mid] > element:
+                high = mid - 1
+            elif self.data[mid] < element:
+                low = mid + 1
+
+
+if __name__ == "__main__":
+    t1 = ArraySearch()
+    arraydata = array.array('i', [2, 3, 6, 8, 9])
+    t1.insertelement(2, 7)
+    print(t1.array)
+    t1.deleteelement(3)
+    print(t1.array)
+    print(t1.linearsearch(-1))
+    print(t1.binarysearchloop(14, 0, 6))
+    print(t1.binarysearchrecursion(14, 0, 6))
+
---
+++ arrayadt/implementarrayadt.py
@@ -0,0 +1,37 @@
+import array
+
+
+class ArrayADT:
+
+    def __init__(self):
+        self.array = array.array("i", [2, 3, 6, 9, 8])
+        pass
+
+    def insertelement(self, index: int, value: int):
+        currentlength = len(self.array)
+        self.array.append(1)
+
+        if index <= currentlength:
+            for i in range(currentlength, index - 1, -1):
+                self.array[i] = self.array[i - 1]
+            self.array[index] = value
+        else:
+            raise IndexError
+
+    def deleteelement(self, index: int):
+        currentlength = len(self.array)
+        if index <= currentlength:
+            for i in range(index, currentlength-1):
+                self.array[i] = self.array[i + 1]
+            self.array.pop()
+        else:
+            raise IndexError
+
+
+if __name__ == "__main__":
+    t1 = ArrayADT()
+    print(t1.array)
+    t1.insertelement(2, 7)
+    print(t1.array)
+    t1.deleteelement(3)
+    print(t1.array)
