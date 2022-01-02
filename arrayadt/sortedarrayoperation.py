89a9c405f2b5de1cf1e06fe49a1e668a5770a028
shubham920816 <shubhaM1674@>
2022-01-02 15:44:58 +0530

    Array

--- arrayadt/sortedarrayoperation.py
+++ arrayadt/sortedarrayoperation.py
@@ -4,7 +4,8 @@ from array import array
 class SortedArrayOperation(object):
 
     def __init__(self):
-        self.array = array('i', [4, 8, 13, 23, 26, 33])
+        # self.array = array('i', [4, 8, 13, 23, 26, 33])
+        self.array = array('i', [-6, 3, -8, 10, 5, -7, -9, 12, 4, 2])
 
     def insertElement(self, element):
         appendCounter = 0
@@ -29,8 +30,52 @@ class SortedArrayOperation(object):
                 value = True
         return value
 
+    def NegativeOnRight(self):
+        j = len(self.array) - 1
+        i = 0
+        for i in range(0, len(self.array)):
+
+            if self.array[i] < 0 and i < j:
+                for y in range(j, 0, -1):
+                    if self.array[y] >= 0:
+                        self.array[y], self.array[i] = self.array[i], self.array[y]
+                        j = j - 1
+                        break
+            elif i > j:
+                break
+        return self.array
+
+    def NegativeOnRightOptimizedApproach(self):
+        j = len(self.array) - 1
+        i = 0
+        while i < j:
+            while self.array[i] >= 0:
+                i = i + 1
+            while self.array[j] < 0:
+                j = j - 1
+            if i < j:
+                self.array[i], self.array[j] = self.array[j], self.array[i]
+
+        return self.array
+
+    def NegativeOnLeft(self):
+        j = len(self.array) - 1
+        i = 0
+        while i < j:
+            while self.array[i] < 0:
+                i = i + 1
+            while self.array[j] >= 0:
+                j = j - 1
+            if i < j:
+                self.array[i], self.array[j] = self.array[j], self.array[i]
+
+        return self.array
+
 
 if __name__ == "__main__":
     t1 = SortedArrayOperation()
     # print(t1.insertElement(34))
-    print(t1.isSorted())
+    # print(t1.isSorted())
+    print(t1.NegativeOnRight())
+    print(t1.NegativeOnLeft())
+    print(t1.NegativeOnRightOptimizedApproach())
