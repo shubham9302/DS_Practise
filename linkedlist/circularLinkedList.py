324621c30eb6c7d30e2fba6ef2b0d00e5ab8ba07
shubham920816 <shubhaM1674@>
2022-03-24 00:05:51 +0530

    linkedlist

---
+++ linkedlist/circularLinkedList.py
@@ -0,0 +1,79 @@
+from array import array
+
+
+class Node:
+    def __init__(self, value=None, next=None):
+        self.data = value
+        self.next = next
+
+
+class CircularLinkedList:
+
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def insertElement(self, value):
+        if self.head is None:
+            self.head = self.tail = Node(value)
+            self.tail.next = self.head
+        else:
+            temp = Node(value)
+            self.tail.next = temp
+            self.tail = self.tail.next
+            self.tail.next = self.head
+
+    def getLength(self):
+        counter = 1
+        initial = self.head
+        temp = initial.next
+        while temp != initial:
+            value = temp.data
+            temp = temp.next
+            counter += 1
+        return counter
+
+    def printList(self):
+        initial = self.head
+        temp = initial.next
+        print(initial.data)
+        while temp != initial:
+            value = temp.data
+            print(value)
+            temp = temp.next
+
+    def insertElementAtPosition(self, value=None, position=None):
+        initial = self.head
+        tail_counter = initial
+        temp = initial.next
+        counter = 1
+        initial_length = self.getLength()
+        if position == 0:
+            next_node_adress = self.head
+            self.head = Node(value, next_node_adress)
+            self.tail.next = self.head
+        elif position >= initial_length:
+            raise IndexError
+
+
+        else:
+            while temp != initial:
+                if counter == position:
+                    next_node_adress = temp
+                    tail_counter.next = Node(value, next_node_adress)
+                    break
+                elif counter < initial_length:
+                    counter += 1
+                    tail_counter = temp
+                    temp = temp.next
+
+
+if __name__ == "__main__":
+    t1 = CircularLinkedList()
+    t1.insertElement(2)
+    t1.insertElement(3)
+    t1.insertElement(7)
+    t1.insertElement(9)
+    print("$$$$$$$$")
+    t1.insertElementAtPosition(5, 0)
+    t1.printList()
