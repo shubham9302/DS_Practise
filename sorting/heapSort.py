d0a617ef2e0dd6b7d229afbf14cfb28e5ce3bcfc
shubham920816 <shubhaM1674@>
2022-06-21 00:44:15 +0530

    Heap Sort

--- sorting/heapSort.py
+++ sorting/heapSort.py
@@ -26,46 +26,43 @@ class HeapSort:
     def _heapDeletion(self, parent_index, end__index):
         left_child_index = 2 * parent_index + 1
         right_child_index = 2 * parent_index + 2
-        right_child = self.tempArray[right_child_index]
-        left_child = self.tempArray[left_child_index]
-        print("end_index", end__index)
-        print("left_child_index", left_child_index)
-        print("right_child_index", right_child_index)
         if left_child_index < end__index and right_child_index < end__index:
+            right_child = self.tempArray[right_child_index]
+            left_child = self.tempArray[left_child_index]
             if left_child > right_child:
                 comparisonIndex = left_child_index
-                comparisonElement = left_child
+                comparisonElement = self.tempArray[left_child_index]
             else:
                 comparisonIndex = right_child_index
-                comparisonElement = right_child
+                comparisonElement = self.tempArray[right_child_index]
             print("comparisonElement", comparisonElement)
             print("comparisonIndex", comparisonIndex)
             end__index = end__index
             parentElement = self.tempArray[parent_index]
-            if comparisonElement > parentElement and comparisonElement is not None:
-                self.tempArray[comparisonIndex], self.tempArray[parent_index] = self.tempArray[parent_index], \
-                                                                                self.tempArray[comparisonIndex]
-                self._heapDeletion(comparisonIndex, end__index)
+            self.recursiveDownwardCall(comparisonElement, parentElement, comparisonIndex, parent_index,
+                                       end__index)
 
         elif left_child_index >= end__index > right_child_index:
             comparisonIndex = right_child_index
-            comparisonElement = right_child
+            comparisonElement = self.tempArray[right_child_index]
             end__index = end__index
             parentElement = self.tempArray[parent_index]
-            if comparisonElement > parentElement and comparisonElement is not None:
-                self.tempArray[comparisonIndex], self.tempArray[parent_index] = self.tempArray[parent_index], \
-                                                                                self.tempArray[comparisonIndex]
-                self._heapDeletion(comparisonIndex, end__index)
+            self.recursiveDownwardCall(comparisonElement, parentElement, comparisonIndex, parent_index,
+                                       end__index)
 
         elif left_child_index < end__index <= right_child_index:
             comparisonIndex = left_child_index
-            comparisonElement = left_child
+            comparisonElement = self.tempArray[left_child_index]
             end__index = end__index
             parentElement = self.tempArray[parent_index]
-            if comparisonElement > parentElement and comparisonElement is not None:
-                self.tempArray[comparisonIndex], self.tempArray[parent_index] = self.tempArray[parent_index], \
-                                                                                self.tempArray[comparisonIndex]
-                self._heapDeletion(comparisonIndex, end__index)
+            self.recursiveDownwardCall(comparisonElement, parentElement, comparisonIndex, parent_index,
+                                       end__index)
+
+    def recursiveDownwardCall(self, comparisonElement, parentElement, comparisonIndex, parent_index, end__index):
+        if comparisonElement > parentElement and comparisonElement is not None:
+            self.tempArray[comparisonIndex], self.tempArray[parent_index] = self.tempArray[parent_index], \
+                                                                            self.tempArray[comparisonIndex]
+            self._heapDeletion(comparisonIndex, end__index)
 
     def deleteElementFromHeap(self):
         end_index = len(self.tempArray) - 1
@@ -77,8 +74,14 @@ class HeapSort:
             end_index -= 1
             print("postForLoop", self.tempArray)
 
-    def heapSort(self):
-        pass
+    def heapify(self):
+        end_index = len(self.arr)
+        self.tempArray = self.arr
+        for i in range(end_index-1, -1, -1):
+            print("i", i)
+            self._heapDeletion(i, end_index)
+            print("postForLoop", self.tempArray)
+        print(self.tempArray)
 
 
 if __name__ == "__main__":
@@ -86,3 +89,21 @@ if __name__ == "__main__":
     t1 = HeapSort(arr)
     t1.insertElementInHeap()
     t1.deleteElementFromHeap()
+    #t1.heapify()
+
+
+###Time complexity
+"""
+Heap Sort consist of 2 parts 
+1.creation of heap - O(nlogn)
+2.deletion from heap - O(nlogn)
+
+Thus total complexity will sum of step1 and step2 and final
+time complexity is  O(nlogn)
+
+Heapify is process of creation of heap by starting from
+end of the array 
+time complexity is O(n)
+
+"""
+
