7db100026b9e74fd8c90c93fa9e51c6f51fe893c
shubham920816 <shubhaM1674@>
2022-09-27 00:13:30 +0530

    Binary Search

--- adhoc/binarySearchIterative.py
+++ adhoc/binarySearchIterative.py
@@ -1,3 +1,14 @@
+""""
+We should write mid as (low + ((high -low)/2)))
+This repersentation helps in reducing overflow
+condition
+if we write mid = (low + high) /2
+and if the array size if very big low + high
+can result in overflow
+
+"""
+
+
 class BinarySearch:
 
     def __init__(self, a):
@@ -19,9 +30,56 @@ class BinarySearch:
                 l = mid + 1
         return position
 
+    def searchElementPositionReverse(self, a, val):
+        l = 0
+        h = len(a) - 1
+        state = False
+        position = "Not Found"
+        while not state and h >= l:
+            mid = int((l + h) / 2)
+            if a[mid] == val:
+                state = True
+                position = mid
+            elif a[mid] > val:
+                l = mid + 1
+            else:
+                h = mid - 1
+        return position
+
+    def firstOcurrence(self, a, val):
+        l = 0
+        h = len(a) - 1
+        res = -1
+        while h >= l:
+            mid = int((l + h) / 2)
+            if self.a[mid] == val:
+                res = mid
+                h = mid - 1
+            elif self.a[mid] > val:
+                h = mid - 1
+            else:
+                l = mid + 1
+        return res
+
+    def lastOcurrence(self, a, val):
+        l = 0
+        h = len(a) - 1
+        res = -1
+        while h >= l:
+            mid = int((l + h) / 2)
+            if self.a[mid] == val:
+                res = mid
+                l = mid + 1
+            elif self.a[mid] > val:
+                h = mid - 1
+            else:
+                l = mid + 1
+        return res
 
 
 if __name__ == "__main__":
     listData = [3, 6, 8, 12, 14, 17, 25, 29.31, 36, 42, 47, 53, 55, 62]
+    reversedData = [listData[i] for i in range(len(listData) - 1, -1, -1)]
     t1 = BinarySearch(listData)
     print(t1.searchElementPosition(-1))
+    print(t1.searchElementPositionReverse(reversedData, 6))
--- patternsDs/slidingWindow.py
+++ patternsDs/slidingWindow.py
@@ -45,3 +45,60 @@ class slidingWindow():
                 initialSum -= arr[FirstVal]
                 FirstVal += 1
             result.append(resultData)
+
+    """
+    Given an array of positive integers and a number ‘S,’ 
+    find the length of the smallest contiguous subarray 
+    whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists
+    Input: [2, 1, 5, 2, 3, 2], S=7
+    Output: 2
+    Explanation: The smallest subarray with a sum greater than or equal to ‘7’ is [5, 2].
+    """
+
+    def bruteForceP2(self, k, arr):
+        minLength = float('inf')
+        for i in range(len(arr)):
+            sum = 0.0
+            counter = 0
+            for y in range(i + 1, len(arr)):
+                sum += arr[y]
+                counter += 1
+                if sum >= k:
+                    if counter < minLength:
+                        minLength = counter
+                    break
+        return minLength
+
+    def optSolutionP2(self, k, arr):
+        minLength = float('inf')
+        sumVal, elementIndex = 0.0, 0
+        steps = 0
+        for i in range(len(arr)):
+            sumVal += arr[i]
+            print("sumVal", sumVal)
+            while sumVal >= k:
+                print("i", i)
+                print("eleementIndex", elementIndex)
+                steps = i - elementIndex + 1
+                sumVal -= arr[elementIndex]
+                elementIndex += 1
+                print("steps", steps)
+                print("sumValReduced", sumVal)
+                print("eleementIndex", elementIndex)
+                print("i", i)
+
+                if steps < minLength:
+                    minLength = steps
+
+                print("minLength", minLength)
+
+            print("#######")
+
+        return minLength
+
+
+if __name__ == "__main__":
+    t1 = slidingWindow()
+    arr = [2, 1, 5, 2, 3, 2]
+    k = 8
+    print(t1.optSolutionP2(k, arr))
